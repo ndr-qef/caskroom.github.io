e544bf6b110576a8c9165eaf5bf09364e1fb3a9c
ndr <ndr@qef.io>
2014-02-05 18:58:01 +0100

    Move js to separate file; add a few small adjustments
    Namely: use jquery 2 (we don't need legacy support); use Google CDN;
    include jquery in body instead of head; use pre-existing classes to
    target counters.

---
+++ cask.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function request(url, spanId) {
+        $.ajax({
+            url: url,
+            type: "GET",
+        success: function(data) {
+              $('#' + spanId).fadeOut(250, function() {
+                  $(this).text(data.length.toString()).fadeIn(250);
+              });
+          }
+        });
+    }
+
+  request("https://api.github.com/repos/phinze/homebrew-cask/contents/Casks",
+          "n-cask");
+  
+  request("https://api.github.com/repos/phinze/homebrew-cask/contributors",
+          "n-contrib");
+})
