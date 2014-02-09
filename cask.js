c698b0fa38577b4cb30ccb48adc326504e0c7813
ndr <ndr@qef.io>
2014-02-09 23:42:47 +0100

    Add search functionality
    Preliminary work.
    Depends on: GitHub API v3, lunr full-text search, doT templating;
    uses HTML5 History API.
    
    It is not currently possible to reach the search page directly.

--- cask.js
+++ cask.js
@@ -1,14 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    function request(url, spanId) {
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var initialUrl = location.href,
+        historyCanary = false;
+    
+    var caskList;
+    
+    var request = function request(url, spanId) {
         $.ajax({
             url: url,
             type: "GET",
-        success: function(data) {
-              $('#' + spanId).fadeOut(250, function() {
-                  $(this).text(data.length.toString()).fadeIn(250);
-              });
-          }
+            success: function(data) {
+                $("#" + spanId).fadeOut(250, function() {
+                    $(this).text(data.length.toString()).fadeIn(250);
+                });
+                
+                if (spanId === "n-cask") { process(data); }
+            }
         });
     }
 
@@ -17,4 +24,73 @@ document.addEventListener('DOMContentLoaded', function() {
   
     request("https://api.github.com/repos/phinze/homebrew-cask/contributors",
           "n-contrib");
-})
+    
+    $("#search-button").on("click", function(e) {
+        $("#landing-page").toggleClass("off-canvas");
+        $("#search-page").toggleClass("off-canvas");
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $("#search-input").focus();
+        history.pushState(null, null, "/");
+        historyCanary = true;
+        e.preventDefault();
+    });
+    
+    window.addEventListener("popstate", function() {
+        var fauxPop = !historyCanary && location.href === initialURL
+        historyCanary = true;
+        if (fauxPop) {
+            return;
+        } else {
+            $("#landing-page").toggleClass("off-canvas");
+            $("#search-page").toggleClass("off-canvas");
+        }
+    });
+    
+    var index = lunr(function() {
+        this.ref("id");
+        this.field("name", 10);
+    });
+    
+    var searchTemplate = $("#search-template").html(),
+        render = doT.template(searchTemplate);
+    
+    var process = function process(data) {
+        caskList = data.map(function(raw, i) {
+            return {
+                id: i,
+                name: raw.name.substr(0, raw.name.lastIndexOf(".")) || raw.name
+            };
+        });
+        
+        caskList.forEach(function(item) {
+            index.add(item);
+        });
+    };
+    
+    var debounce = function debounce(fn) {
+        var timeout;
+        
+        return function () {
+            var args = Array.prototype.slice.call(arguments),
+                ctx = this;
+
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                fn.apply(ctx, args);
+            }, 200);
+        };
+    };
+
+    $("#search-input").on("keyup", debounce(function() {
+        if ($(this).val() < 1) {
+            $("#search-view").html("<div class=\"search-item no-basis delta ale highlight-bg\">Search for an app.</div>");    
+        } else {
+            var query = $(this).val();
+            var results = index.search(query).map(function(result) {
+                return caskList.filter(function(q) { return q.id === parseInt(result.ref, 10) })[0]
+            });
+
+            $("#search-view").html(render(results));
+        }
+    }));
+});
